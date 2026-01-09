6091a50ceb39fd9e71d1f1233cede6bce5c514f6
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2026-01-09 12:45:04 +0700

    backend init

---
+++ backend/src/main/java/com/khanh/SpringSecrurityJwt/SpringSecrurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.khanh.SpringSecrurityJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecrurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecrurityJwtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/khanh/SpringSecrurityJwt/SpringSecrurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanh.SpringSecrurityJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecrurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
