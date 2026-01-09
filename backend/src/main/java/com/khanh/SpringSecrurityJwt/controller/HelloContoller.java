924e0e73b83353a26b60520c0781702461db7a15
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2026-01-09 15:02:41 +0700

    models updated

---
+++ backend/src/main/java/com/khanh/SpringSecrurityJwt/controller/HelloContoller.java
@@ -0,0 +1,15 @@
+package com.khanh.SpringSecrurityJwt.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContoller {
+
+    @GetMapping("")
+    public String greet(HttpServletRequest request) {
+        return "Welcome to Spring Boot" + request.getSession().getId();
+    }
+
+}
